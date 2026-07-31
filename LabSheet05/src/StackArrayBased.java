7768332ee3b55492624279b5e35f2171c1ac83a9
Woraya Miankerd <2513510038@tni.ac.th>
2026-07-31 11:09:22 +0700

    2513510038 submit LabSheet05

---
+++ LabSheet05/src/StackArrayBased.java
@@ -0,0 +1,55 @@
+public class StackArrayBased {
+	
+   private int[] stacks;	 	// array to store elements
+   private final int MAX_STACK = 50;	// maximum size of stack
+   private int top;		 	// index of the top element
+
+   // create an empty stack with a maximum size
+   public StackArrayBased() {
+      stacks = new int[MAX_STACK];
+      top = -1;
+   }
+
+   // create an empty stack with a specific size
+   public StackArrayBased(int capacity) {
+      stacks = new int[capacity];
+      top = -1;
+   }
+   
+   public boolean isEmpty() {
+	   return top == -1;
+   }
+   
+   public boolean isFull() {
+	   return top == stacks.length-1;
+	   // return true = full
+	   // return false = not full
+   }
+   
+   public boolean push(int new_element) {
+	   if(!isFull()) {
+		   stacks[++top] = new_element;
+		   return true;
+	   }
+	   return false;
+   }
+   
+   public int peek() {
+	   if(!isEmpty()) {
+		   return stacks[top];
+	   }
+	   return -1; // stacks is empty
+   }
+   
+   public int pop() {
+	   if (!isEmpty()) {
+		   return stacks[top--]; // return แล้วลบออก
+	   }
+	   return -1;
+   }
+   
+   public void popAll() { // หวนสู่ค่าเริ่มต้น
+	   stacks = new int[MAX_STACK];
+	   top = -1;
+   }
+}
